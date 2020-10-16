4e3f8dca2b1eaacc75eae4ff3e4b825ed67b969f
Peter G.A. Barraud <barraud@adobe.com>
2020-10-16 22:15:51 +0530

    isExcangeRequestActive and send notification

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -43,7 +43,8 @@ export default class WelcomeScreen extends Component {
           last_name:this.state.lastName,
           mobile_number:this.state.mobileNumber,
           username:this.state.username,
-          address:this.state.address
+          address:this.state.address,
+          isRequestActive:false
         })
         return  Alert.alert(
              'User Added Successfully',
